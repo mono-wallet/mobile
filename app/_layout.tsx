a8824c04a5023316cd0cb98f65dafc0d3c3f5dd6
Max Levchuk <mlevchuk@yukon.cv.ua>
2024-06-12 22:48:14 +0300

    [ADDED] i18n

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,15 +2,19 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import 'react-native-reanimated';
+import { Text } from 'react-native';
+import i18n from '@/i18n';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+import {I18nextProvider, useTranslation} from 'react-i18next';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const { t } = useTranslation();
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
@@ -28,10 +32,13 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
+      <I18nextProvider i18n={i18n}>
+        <Text>{t("login.title")}</Text>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="+not-found" />
+        </Stack>
+      </I18nextProvider>
     </ThemeProvider>
   );
 }
