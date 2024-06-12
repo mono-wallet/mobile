efb0243cedf5fdf5e1606377ec3fd67b8a746e11
Max Levchuk <mlevchuk@yukon.cv.ua>
2024-06-12 23:44:09 +0300

    [ADDED] SafeArea [ADDED] KeyboardAvoiding

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,21 +1,27 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import React, { useEffect } from 'react';
-import 'react-native-reanimated';
-import i18n from '@/i18n';
-
-import { useColorScheme } from '@/hooks/useColorScheme';
-import {I18nextProvider} from 'react-i18next';
-import ReactQuery from '@/context/reactQuery';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import React, { useEffect } from "react";
+import "react-native-reanimated";
+import i18n from "@/i18n";
+import { useColorScheme } from "@/hooks/useColorScheme";
+import { I18nextProvider } from "react-i18next";
+import ReactQuery from "@/context/ReactQuery";
+import KeyboardAvoiding from "@/context/KeyboardAvoiding";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { TextInput } from "react-native";
 
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
 
   useEffect(() => {
@@ -29,13 +35,17 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <I18nextProvider i18n={i18n}>
         <ReactQuery>
-          <Stack>
-            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-            <Stack.Screen name="+not-found" />
-          </Stack>
+          <SafeAreaProvider>
+            <KeyboardAvoiding>
+              <Stack>
+                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+                <Stack.Screen name="+not-found" />
+              </Stack>
+            </KeyboardAvoiding>
+          </SafeAreaProvider>
         </ReactQuery>
       </I18nextProvider>
     </ThemeProvider>
