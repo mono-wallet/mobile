1dcea138021ad88f0da8675f62733e5354a4cecd
unisol1020 <unisol1020@icloud.com>
2024-07-14 14:25:01 +0300

    [ADDED] basic components

---
+++ components/Text.tsx
@@ -0,0 +1,24 @@
+import {ComponentPropsWithoutRef, createContext, ElementRef, forwardRef, useContext} from 'react';
+import * as React from 'react';
+import { Text as RNText } from 'react-native';
+import { cn } from '../lib/utils';
+
+const TextClassContext = createContext<string | undefined>(undefined);
+
+const Text = forwardRef<ElementRef<typeof RNText>, ComponentPropsWithoutRef<typeof RNText>>(
+  ({ className = false, ...props }, ref) => {
+    const textClass = useContext(TextClassContext);
+
+    return (
+      <RNText
+        className={cn('text-base text-foreground web:select-text', textClass, className)}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+
+Text.displayName = 'Text';
+
+export { Text, TextClassContext };
