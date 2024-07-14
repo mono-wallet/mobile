dd09ac3d64aa08a0dcfb2cf255fdb714a368a43d
unisol1020 <unisol1020@icloud.com>
2024-07-14 14:24:23 +0300

    [ADDED] necessary libraries

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,31 +1,30 @@
+import "../global.css";
 import {
   DarkTheme,
   DefaultTheme,
   ThemeProvider,
 } from "@react-navigation/native";
 import { useFonts } from "expo-font";
-import { Slot, Stack } from "expo-router";
+import { Slot } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import React, { useEffect } from "react";
 import "react-native-reanimated";
-import { useColorScheme } from "@/hooks/useColorScheme";
 import { I18nextProvider } from "react-i18next";
 import ReactQuery from "@/context/ReactQuery";
 import KeyboardAvoiding from "@/context/KeyboardAvoiding";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { View, Text } from "react-native";
-import { LogBox } from "react-native";
+import { useColorScheme as NWUseColorScheme } from "nativewind";
+import { useColorScheme } from "react-native";
 import i18n from "@/i18n";
-
-LogBox.ignoreLogs([/^Cannot update a component*/]);
 
 SplashScreen.preventAutoHideAsync();
 
 export const unstable_settings = {
-  initialRouteName: "login/",
+  initialRouteName: "login",
 };
 
 const RootLayout = () => {
+  const { setColorScheme } = NWUseColorScheme();
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
@@ -35,14 +34,12 @@ const RootLayout = () => {
     if (loaded) {
       SplashScreen.hideAsync();
     }
+
+    setColorScheme(colorScheme ?? "dark");
   }, [loaded]);
 
   if (!loaded) {
-    return (
-      <View>
-        <Text>Loading...</Text>
-      </View>
-    );
+    return null;
   }
 
   return (
